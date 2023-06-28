5b782cc94b1445be2c737645881f9bc752eb5d33
Jamshid <hushdilcss@gmail.com>
2023-06-29 01:12:49 +0300

    asa eklendi, aim çalışması yarıda kaldı.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")] public float movementSpeed;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    private bool _canJump = true;
+    private bool _isFalling;
+
+    public Transform orientation;
+    public Animator _animator;
+
+    private float _horizontalInput;
+    private float _verticalInput;
+    private Vector3 _moveDirection;
+    private Rigidbody _rb;
+    private bool _isGrounded;
+
+    public float playerHeight;
+    public LayerMask groundMask;
+    public float groundDrag;
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+    private static readonly int IsJumping = Animator.StringToHash("isJumping");
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, groundMask);
+        _isFalling = _rb.velocity.y < 0.1f;
+        MyInput();
+        SpeedControl();
+
+        if (_isGrounded)
+        {
+            _rb.drag = groundDrag;
+        }
+        else
+        {
+            _rb.drag = 0;
+        }
+
+        
+    }
+
+    private void MyInput()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+        if (_horizontalInput != 0 || _verticalInput != 0)
+        {
+            _animator.SetBool(IsWalking, true);
+        }
+        else
+        {
+            _animator.SetBool(IsWalking, false);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && _canJump && _isGrounded)
+        {
+            Jump();
+            _canJump = false;
+            ResetJump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _moveDirection = orientation.forward * _verticalInput + orientation.right * _horizontalInput;
+
+        if (_isGrounded)
+        {
+            // Movement
+            _rb.velocity = new Vector3(_moveDirection.x * movementSpeed, _rb.velocity.y, _moveDirection.z * movementSpeed);
+            
+        }
+        else if (!_isGrounded)
+        {
+            // Air movement
+            _rb.velocity = new Vector3(_moveDirection.x * movementSpeed, _rb.velocity.y, _moveDirection.z * movementSpeed);
+            if (_rb.velocity.y < 0)
+            {
+                // Falling
+                _animator.SetBool(IsJumping,false);
+                _rb.velocity += Vector3.up * (Physics.gravity.y * (airMultiplier - 1) * Time.deltaTime);
+            }
+        }
+          
+        
+    }
+
+    private void SpeedControl()
+    {
+        var flatVelocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
+        if (flatVelocity.magnitude > movementSpeed)
+        {
+            var newVelocity = flatVelocity.normalized * movementSpeed;
+            _rb.velocity = new Vector3(newVelocity.x, _rb.velocity.y, newVelocity.z);
+        }
+    }
+
+    public bool IsGrounded()
+    {
+        return _isGrounded;
+    }
+
+    private void Jump()
+    {
+        _rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+
+        _rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        
+        _animator.SetBool(IsJumping, true);
+    }
+
+    private void ResetJump()
+    {
+        _canJump = true;
+    }
+}
--- Assets/Scripts/ThirdPersonCam.cs
+++ Assets/Scripts/ThirdPersonCam.cs
@@ -3,14 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CameraStyle
+{
+    Basic,
+    Combat
+}
 public class ThirdPersonCam : MonoBehaviour
 {
     public Transform orientation;
+    public Transform combatOrientation;
     public Transform player;
     public Transform playerObject;
-    public Rigidbody rb;
     public float rotationSpeed;
-    public PlayerMovement playerMovement;
+    public CameraStyle currentStyle;
+    public GameObject basicCam;
+    public GameObject combatCam;
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -20,16 +27,27 @@ public class ThirdPersonCam : MonoBehaviour
     private void Update()
     {
         var viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
-        orientation.forward = viewDir.normalized;
-
         var horizontalInput = Input.GetAxis("Horizontal");
         var verticalInput = Input.GetAxis("Vertical");
         var inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        
-        if(inputDir.magnitude > 0.1f)
+        if(Input.GetMouseButton(1))
         {
+            basicCam.SetActive(false);
+            combatCam.SetActive(true);
+            // var dirToCombatOrientation =  combatOrientation.position - new Vector3(transform.position.x, combatOrientation.position.y, transform.position.z);
+            // orientation.forward = dirToCombatOrientation.normalized;
             playerObject.forward = Vector3.Slerp(playerObject.forward, inputDir.normalized, rotationSpeed * Time.deltaTime * rotationSpeed);
-
         }
+        else
+        {
+            basicCam.SetActive(true);
+            combatCam.SetActive(false);
+            if (inputDir.magnitude > 0.1f)
+            {
+                playerObject.forward = Vector3.Slerp(playerObject.forward, inputDir.normalized, rotationSpeed * Time.deltaTime * rotationSpeed);
+            }
+        }
+        orientation.forward = viewDir.normalized;
+
     }
 }
