68584c2fa7f0cd8024db5d37bfbe0a4389724b17
Beyza Nur Bayrakdaroğlu <119537892+beyzanurbayrakdaroglu@users.noreply.github.com>
2023-07-16 00:30:50 +0300

    BookUpAnim

--- Assets/PlayerStart.cs
+++ Assets/PlayerStart.cs
@@ -6,16 +6,14 @@ using UnityEngine.Playables;
 public class PlayerStart : MonoBehaviour
 {
   [SerializeField]  private PlayableDirector _playableDirevtor;
-    void Start()
-    {
-       
-    }
+  public Animator BookUpAnim;
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            BookUpAnim.SetTrigger("BookUp");
             _playableDirevtor.Play();
         }
     }
