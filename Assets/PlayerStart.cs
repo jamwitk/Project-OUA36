4990cc82a9e2663b6e6c136ac7f04070a06743db
Beyza Nur Bayrakdaroğlu <beyza.byrkdrgl@gmail.com>
2023-07-16 11:04:40 +0300

    ses eklendi

--- Assets/PlayerStart.cs
+++ Assets/PlayerStart.cs
@@ -7,18 +7,24 @@ public class PlayerStart : MonoBehaviour
 {
   [SerializeField]  private PlayableDirector _playableDirevtor;
   public Animator BookUpAnim;
-
+  AudioSource audioSource;
+  
+  
     // Update is called once per frame
+     void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+      
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
-        {
+        {       
+            audioSource.Play();
+            
             BookUpAnim.SetTrigger("BookUp");
             _playableDirevtor.Play();
         }
     }
-    void Update()
-    {
-        
-    }
+  
 }
